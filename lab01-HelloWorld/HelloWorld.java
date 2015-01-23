cf3dc417fb2119d95e5017f95c23704b5a02bbea
tcl217 <tcl217@lehigh.edu>
2015-01-23 19:50:46 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//Teresita Liebel
+//lab01 
+//Hello World Java Program
+
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("#                    #");
+        System.out.println("# Hellow World 2014! #");
+        System.out.println("#                    #");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("test");
+        
+        
+    }
+
+}
