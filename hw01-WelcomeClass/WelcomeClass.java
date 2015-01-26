da07c2a5659e11cfe267055ee425ad073b108834
tcl217 <tcl217@lehigh.edu>
2015-01-26 04:00:21 +0000

    updated Hello World

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Teresita Liebel
+//hw01- Welcome Class Java Program
+//January 25,2015
+
+public class WelcomeClass{
+    
+    public static void main(String [] args) {
+        
+        System.out.println("   -----------");
+        System.out.println("   | Welcome |");
+        System.out.println("   -----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \/ \/ \/ \/ \/ \");
+        System.out.println("-T--C--L--2--1--7-");
+        System.out.println("\ /\ /\ /\ /\ /\ /");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("I am the oldest of two girls and this summer I have an internship in Chicago.");
+        
+    }
+    
+    
+    
+    
+}
