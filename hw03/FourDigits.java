25a2777f5db6c720734cae475ec000507848f664
tcl217 <tcl217@lehigh.edu>
2015-02-10 17:28:45 +0000

    Final completed-3 programsls

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -21,18 +21,26 @@ import java.util.Scanner;
                 System.out.print("Enter a double and I display the four digits to the right of the decimal point- ");
             
                 //accept user input
-                double nDouble= myScanner.nextDouble();
+                double amount= myScanner.nextDouble();
               
                      //cast double to int
-                     int nInt= (int)nDouble;
-              
-                     //declare variables
-                     int nInt2= nInt*10000;
-                     int nInt3= (int)(nDouble*10000);
-            
-                
+                     
+                     int remainingAmount1=(int)(amount*10000);
+                    
+                     int numberA= remainingAmount1%10000;
+                     numberA= numberA/1000;
+                     
+                     int numberB=remainingAmount1%1000;
+                     numberB=numberB/100;
+                     
+                     int numberC=remainingAmount1%100;
+                     numberC=numberC/10;
+                     
+                     int numberD=remainingAmount1%10;
+                     numberD= numberD/1;
+                    
                         //set up output of the four digits
-                        System.out.println("The four digits are " + (nInt3-nInt2));
+                        System.out.println("The four digits are " + numberA +numberB +numberC +numberD);
             
             
             
